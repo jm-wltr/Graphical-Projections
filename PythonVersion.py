31b56cbfbdcaf85561c2ca074908c2198a5d226c
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-09-12 11:29:44 +0200

    Added a slider for phi that doesn't do anything

--- PythonVersion.py
+++ PythonVersion.py
@@ -15,44 +15,105 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
+from matplotlib.widgets import Slider, Button
 
-# Coordinates of vertices
-vertices = np.array([[-1, -1, -1],
-                  [1, -1, -1 ],
-                  [1, 1, -1],
-                  [-1, 1, -1],
-                  [-1, -1, 1],
-                  [1, -1, 1 ],
-                  [1, 1, 1],
-                  [-1, 1, 1]])
-                       
 # Define rotation matrices
-phi = 0
+phi = 2
 theta = 0
 psi = 0
 
-XrollMatrix = np.mat([[1, 0, 0], \
-                      [0, math.cos(phi), math.sin(phi)], \
-                      [0, -math.sin(phi), math.cos(phi)]])
+def rotate(phi, theta, psi):
+    # Coordinates of vertices
+    vertices = np.array([[-1, -1, -1],
+                      [1, -1, -1 ],
+                      [1, 1, -1],
+                      [-1, 1, -1],
+                      [-1, -1, 1],
+                      [1, -1, 1 ],
+                      [1, 1, 1],
+                      [-1, 1, 1]])
     
-YpitchMatrix = np.mat([[math.cos(theta), 0, -math.sin(theta)], \
-                      [0, 1, 0], \
-                      [math.sin(theta), 0, math.cos(theta)]])
-                       
-ZyawMatrix = np.mat([[math.cos(psi), math.sin(psi), 0], \
-                      [-math.sin(psi), math.cos(psi), 0], \
-                      [0, 0, 1]])
-                       
-# Rotate the cube
-Z = np.zeros((8,3))
-for i in range(8): Z[i,:] = np.dot(vertices[i,:],XrollMatrix)
-for i in range(8): Z[i,:] = np.dot(Z[i,:],YpitchMatrix)
-for i in range(8): Z[i,:] = np.dot(Z[i,:],ZyawMatrix)
+    XrollMatrix = np.mat([[1, 0, 0], \
+                          [0, math.cos(phi), math.sin(phi)], \
+                          [0, -math.sin(phi), math.cos(phi)]])
+        
+    YpitchMatrix = np.mat([[math.cos(theta), 0, -math.sin(theta)], \
+                          [0, 1, 0], \
+                          [math.sin(theta), 0, math.cos(theta)]])
+                           
+    ZyawMatrix = np.mat([[math.cos(psi), math.sin(psi), 0], \
+                          [-math.sin(psi), math.cos(psi), 0], \
+                          [0, 0, 1]])
+                           
+    # Rotate the cube
+    Z = np.zeros((8,3))
+    for i in range(8): Z[i,:] = np.dot(vertices[i,:],XrollMatrix)
+    for i in range(8): Z[i,:] = np.dot(Z[i,:],YpitchMatrix)
+    for i in range(8): Z[i,:] = np.dot(Z[i,:],ZyawMatrix)
+    
+    # Plot the edges and faces
+    faces = [[Z[0],Z[1],Z[2],Z[3]],
+      [Z[4],Z[5],Z[6],Z[7]], 
+      [Z[0],Z[1],Z[5],Z[4]], 
+      [Z[2],Z[3],Z[7],Z[6]], 
+      [Z[1],Z[2],Z[6],Z[5]],
+      [Z[4],Z[7],Z[3],Z[0]]]
+    
+    # Orthographic views
+    ortXY = np.mat([[1, 0, 0], \
+                   [0, 1, 0]])
+    ortXZ = np.mat([[1, 0, 0], \
+                    [0, 0, 1]])
+    ortYZ = np.mat([[0, 1, 0], \
+                    [0, 0, 1]])
+    
+    XY = np.zeros((8,2))
+    for i in range(8): XY[i,:] = np.dot(ortXY, Z[i,:])
+    #XY = np.delete(XY, 2, 1)
 
+    XZ = np.zeros((8,2))
+    for i in range(8): XZ[i,:] = np.dot(ortXZ, Z[i,:])
+    #XZ = np.delete(XZ, 1, 1)
 
-## Make the plot
+    YZ = np.zeros((8,2))
+    for i in range(8): YZ[i,:] = np.dot(ortYZ, Z[i,:])
+    #YZ = np.delete(YZ, 0, 1)
+    
+    facesXY = [[XY[7],XY[4],XY[5],XY[6]],
+      [XY[1],XY[2],XY[3],XY[0]], 
+      [XY[4],XY[0],XY[3],XY[7]], 
+      [XY[5],XY[1],XY[2],XY[6]], 
+      [XY[7],XY[3],XY[2],XY[6]],
+      [XY[0],XY[4],XY[5],XY[1]]]
+    facesXZ = [[XZ[7],XZ[4],XZ[5],XZ[6]],
+      [XZ[1],XZ[2],XZ[3],XZ[0]], 
+      [XZ[4],XZ[0],XZ[3],XZ[7]], 
+      [XZ[5],XZ[1],XZ[2],XZ[6]], 
+      [XZ[7],XZ[3],XZ[2],XZ[6]],
+      [XZ[0],XZ[4],XZ[5],XZ[1]]]
+    facesYZ = [[YZ[7],YZ[4],YZ[5],YZ[6]],
+      [YZ[1],YZ[2],YZ[3],YZ[0]], 
+      [YZ[4],YZ[0],YZ[3],YZ[7]], 
+      [YZ[5],YZ[1],YZ[2],YZ[6]], 
+      [YZ[7],YZ[3],YZ[2],YZ[6]],
+      [YZ[0],YZ[4],YZ[5],YZ[1]]]
+    
+    return Z, XY, XZ, YZ, facesXY, facesXZ, facesYZ, faces
+    
+
+results = rotate(phi, theta, psi)
+Z = results[0]
+XY = results[1]
+XZ = results[2]
+YZ = results[3]
+facesXY = results[4]
+facesXZ = results[5]
+facesYZ = results[6]
+faces = results[7]
+
+ ## Make the plot
 fig = plt.figure()
-ax = fig.add_subplot(221, projection='3d', proj_type = "ortho", azim = 45, elev = 35, title="Isometric")
+ax = fig.add_subplot(221, projection='3d', proj_type = "ortho", azim = -45, elev = 35, title="Isometric")
 ax.set_box_aspect([1,1,1])
 
 ax.set_xlabel('X')
@@ -67,38 +128,7 @@ ax.locator_params(axis='x', nbins=3)
 ax.locator_params(axis='y', nbins=3)
 ax.locator_params(axis='z', nbins=3)
 
-# Plot the vertices
-ax.scatter3D(Z[:, 0], Z[:, 1], Z[:, 2])
 
-# Plot the edges and faces
-faces = [[Z[0],Z[1],Z[2],Z[3]],
-  [Z[4],Z[5],Z[6],Z[7]], 
-  [Z[0],Z[1],Z[5],Z[4]], 
-  [Z[2],Z[3],Z[7],Z[6]], 
-  [Z[1],Z[2],Z[6],Z[5]],
-  [Z[4],Z[7],Z[3],Z[0]]]
-
-ax.add_collection3d(Poly3DCollection(faces, facecolors='cyan', linewidths=1, edgecolors='blue', alpha=.25))
-
-# Orthographic views
-ortXY = np.mat([[1, 0, 0], \
-               [0, 1, 0]])
-ortXZ = np.mat([[1, 0, 0], \
-                [0, 0, 1]])
-ortYZ = np.mat([[0, 1, 0], \
-                [0, 0, 1]])
-
-XY = np.zeros((8,2))
-for i in range(8): XY[i,:] = np.dot(ortXY, Z[i,:])
-#XY = np.delete(XY, 2, 1)
-XZ = np.zeros((8,2))
-for i in range(8): XZ[i,:] = np.dot(ortXZ, Z[i,:])
-#XZ = np.delete(XZ, 1, 1)
-YZ = np.zeros((8,2))
-for i in range(8): YZ[i,:] = np.dot(ortYZ, Z[i,:])
-#YZ = np.delete(YZ, 0, 1)
-
-# Plot 2D views
 ax2 = fig.add_subplot(222, title="Top")
 ax2.set_aspect('equal', adjustable='box')
 ax3 = fig.add_subplot(224, title="Front")
@@ -120,39 +150,39 @@ ax3.set_ylabel('Z')
 ax4.set_xlabel('Y')
 ax4.set_ylabel('Z')
 
-ax2.scatter(XY[:, 0], XY[:, 1])
-ax3.scatter(XZ[:, 0], XZ[:, 1])
-ax4.scatter(YZ[:, 0], YZ[:, 1])
+plt.subplots_adjust(bottom=0.25)
 
-facesXY = [[XY[7],XY[4],XY[5],XY[6]],
-  [XY[1],XY[2],XY[3],XY[0]], 
-  [XY[4],XY[0],XY[3],XY[7]], 
-  [XY[5],XY[1],XY[2],XY[6]], 
-  [XY[7],XY[3],XY[2],XY[6]],
-  [XY[0],XY[4],XY[5],XY[1]]]
-facesXZ = [[XZ[7],XZ[4],XZ[5],XZ[6]],
-  [XZ[1],XZ[2],XZ[3],XZ[0]], 
-  [XZ[4],XZ[0],XZ[3],XZ[7]], 
-  [XZ[5],XZ[1],XZ[2],XZ[6]], 
-  [XZ[7],XZ[3],XZ[2],XZ[6]],
-  [XZ[0],XZ[4],XZ[5],XZ[1]]]
-facesYZ = [[YZ[7],YZ[4],YZ[5],YZ[6]],
-  [YZ[1],YZ[2],YZ[3],YZ[0]], 
-  [YZ[4],YZ[0],YZ[3],YZ[7]], 
-  [YZ[5],YZ[1],YZ[2],YZ[6]], 
-  [YZ[7],YZ[3],YZ[2],YZ[6]],
-  [YZ[0],YZ[4],YZ[5],YZ[1]]]
-
-for i in range(6):
-    poly = Polygon(facesXY[i], facecolor='cyan', edgecolor='blue', alpha = 0.25)
-    ax2.add_patch(poly)
-for i in range(6):
-    poly = Polygon(facesXZ[i], facecolor='cyan', edgecolor='blue', alpha = 0.25)
-    ax3.add_patch(poly)
-for i in range(6):
-    poly = Polygon(facesYZ[i], facecolor='cyan', edgecolor='blue', alpha = 0.25)
-    ax4.add_patch(poly)
-    
 # Show plot
 plt.subplots_adjust(wspace = 0.5, hspace = 0.5)
 plt.show()
+
+def plot(Z, XY, XZ, YZ, facesXY, facesXZ, facesYZ, faces):
+    ax.scatter3D(Z[:, 0], Z[:, 1], Z[:, 2])
+
+    ax.add_collection3d(Poly3DCollection(faces, facecolors='cyan', linewidths=1, edgecolors='blue', alpha=.25))
+
+    ax2.scatter(XY[:, 0], XY[:, 1])
+    ax3.scatter(XZ[:, 0], XZ[:, 1])
+    ax4.scatter(YZ[:, 0], YZ[:, 1])
+    
+    for i in range(6):
+        poly = Polygon(facesXY[i], facecolor='cyan', edgecolor='blue', alpha = 0.25)
+        ax2.add_patch(poly)
+    for i in range(6):
+        poly = Polygon(facesXZ[i], facecolor='cyan', edgecolor='blue', alpha = 0.25)
+        ax3.add_patch(poly)
+    for i in range(6):
+        poly = Polygon(facesYZ[i], facecolor='cyan', edgecolor='blue', alpha = 0.25)
+        ax4.add_patch(poly)
+    
+plot(Z, XY, XZ, YZ, facesXY, facesXZ, facesYZ, faces)
+    
+axphi = plt.axes([0.19, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+phi_slider = Slider(
+    ax=axphi,
+    label='Phi [rad]',
+    valmin=0,
+    valmax=3,
+    valinit=phi,
+)
+
